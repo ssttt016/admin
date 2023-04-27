3b1edb59b0ab4dff264065080a3092f77884ec73
ssttt016 <ssttt016@gmail.com>
2023-04-27 09:24:02 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,30 @@
+package com.kbstar.controller;
+
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonArrayFormatVisitor;
+import org.json.simple.JSONArray;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Random;
+
+@Controller
+public class MainController {
+    @RequestMapping("")
+    public String main(){
+        return "index";
+    }
+    @RequestMapping("/index")
+    public String index(Model model){
+        model.addAttribute("center","dashboard");
+        return "index";
+    }
+    @RequestMapping("/livechart")
+    public String livechart(Model model){
+        model.addAttribute("center","livechart");
+        return "index";
+    }
+
+
+
+}
